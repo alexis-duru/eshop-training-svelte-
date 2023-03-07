113398a2e32bda7b969ede3305771b68ec01c747
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-03-07 22:17:45 +0100

    [fix] remove unused functions

--- src/lib/ProductListItem.svelte
+++ src/lib/ProductListItem.svelte
@@ -1,15 +1,20 @@
 <script>
-  import { addItem } from "./store.js";
+  import { addItem } from "../store.js";
+
   export let product;
+
   let quantity = 1;
+
   const increment = () => {
     quantity += 1;
   };
+
   const decrement = () => {
     if (quantity > 1) {
       quantity -= 1;
     }
   };
+
   const addToCart = () => {
     const item = {
       id: product.id,
@@ -17,22 +22,26 @@
       price: product.price,
       quantity: quantity,
     };
+
     addItem(item);
     quantity = 1;
   };
 </script>
 
-<div class="product">
-  <div class="product-info">
-    <h3>{product.title}</h3>
-    <p class="tags">{product.tags.join(", ")}</p>
-    <p class="description">{product.description}</p>
-    <p class="price">{product.price} €</p>
-    <div class="quantity-selector">
-      <button on:click={decrement}>-</button>
-      <input type="number" bind:value={quantity} min="0" />
-      <button on:click={increment}>+</button>
-    </div>
-    <button on:click={addToCart}>Add to cart</button>
+<li class="product-list-item" data-aos="fade-up">
+  <p>{product.id}</p>
+  <h2>{product.title}</h2>
+  <p class="price">{product.price} €</p>
+  <p class="description">{product.description}</p>
+  <div class="quantity-selector">
+    <button on:click={decrement}>-</button>
+    <input type="number" bind:value={quantity} min="0" />
+    <button on:click={increment}>+</button>
   </div>
-</div>
+  <button on:click={addToCart}>Add to cart</button>
+  <h4>
+    {#each product.tags as tag}
+      <span class="tags">{tag}</span>
+    {/each}
+  </h4>
+</li>
