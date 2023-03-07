082d6932866ccb2ba8499aeff57c999c1c9ecd5e
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-03-07 22:16:50 +0100

    [ADDON] add classes on productList component

--- src/lib/ProductList.svelte
+++ src/lib/ProductList.svelte
@@ -1,11 +1,14 @@
 <script>
   import ProductListItem from "./ProductListItem.svelte";
   import products from "../data/eshop.json";
-  // export let products;
 </script>
 
-<div class="product-list">
-  {#each products as product}
-    <ProductListItem {product} />
-  {/each}
+<div class="product-list-container">
+  <div class="product-list">
+    <ul class="product-list-wrapper">
+      {#each products as product}
+        <ProductListItem {product} />
+      {/each}
+    </ul>
+  </div>
 </div>
