c7c9fef4eaa29d64a8b817ef0035a1985b5f90de
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-03-09 23:02:02 +0100

    [fix] change value in input quantity

---
+++ src/lib/QuantityControl.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let quantity;
+  export let min = 1;
+  export let max = 100;
+
+  const handleInput = (event) => {
+    const value = parseInt(event.target.value);
+    quantity = isNaN(value) ? 1 : Math.min(Math.max(min, value), max);
+  };
+
+  const increment = () => {
+    quantity = Math.min(quantity + 1, max);
+  };
+
+  const decrement = () => {
+    quantity = Math.max(quantity - 1, min);
+  };
+</script>
+
+<div class="quantity-picker">
+  <button on:click={decrement}>-</button>
+  <input type="number" value={quantity} {min} {max} on:input={handleInput} />
+  <button on:click={increment}>+</button>
+</div>
